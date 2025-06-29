2417570ef13d8db78bc0b66022e35031b3e5f681
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-06-29 17:19:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,8 @@
-'use server';
-
 import { initializeApp, getApps } from 'firebase-admin/app';
 import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
-// Este padrão garante que o aplicativo Firebase Admin seja inicializado apenas uma vez.
+// This pattern ensures that the Firebase Admin app is initialized only once.
 if (getApps().length === 0) {
   initializeApp();
 }
