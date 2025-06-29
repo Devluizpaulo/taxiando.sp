50561118f372b9d288a42b58b1abe4d4ecb5e5ca
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-06-29 16:06:23 +0000

    Temos esse além de um erro no input

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,14 +1,13 @@
-import { initializeApp, getApps, App, applicationDefault } from 'firebase-admin/app';
+import { initializeApp, getApps, App } from 'firebase-admin/app';
 import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
 let app: App;
 
 if (getApps().length === 0) {
-    // In a Google-managed environment, applicationDefault() should find credentials.
-    app = initializeApp({
-        credential: applicationDefault(),
-    });
+    // In a Google-managed environment, initializeApp() with no arguments 
+    // should find the default credentials.
+    app = initializeApp();
 } else {
     app = getApps()[0];
 }
