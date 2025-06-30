966a3d0fbc6b4f74f4ec245dd00455941ce28557
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-06-30 04:51:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,14 +3,18 @@ import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 import { getStorage } from 'firebase-admin/storage';
 
+let app: admin.app.App;
+
 if (admin.apps.length === 0) {
-    admin.initializeApp({
+    app = admin.initializeApp({
         storageBucket: 'taxiandosp-519b1.appspot.com'
     });
+} else {
+    app = admin.app();
 }
 
-const adminDB = getFirestore();
-const adminAuth = getAuth();
-const adminStorage = getStorage();
+const adminDB = getFirestore(app);
+const adminAuth = getAuth(app);
+const adminStorage = getStorage(app);
 
 export { adminAuth, adminDB, adminStorage, Timestamp };
