3dd6f20051c77f388f3f002cae9a855ec7d6af83
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-06-30 04:49:34 +0000

    isso não esta funcioanoando oque podemo melhorar?????

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,16 @@
 import admin from 'firebase-admin';
 import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
+import { getStorage } from 'firebase-admin/storage';
 
 if (admin.apps.length === 0) {
-    admin.initializeApp();
+    admin.initializeApp({
+        storageBucket: 'taxiandosp-519b1.appspot.com'
+    });
 }
 
 const adminDB = getFirestore();
 const adminAuth = getAuth();
+const adminStorage = getStorage();
 
-export { adminAuth, adminDB, Timestamp };
+export { adminAuth, adminDB, adminStorage, Timestamp };
