e2bf550b4115989b0f6d646fe2583da38852554f
Luiz Paulo <luiz14paulo@hotmail.com>
2025-07-18 15:00:38 -0300

    correção do supabas

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://vzcbqtowoousgklffnhi.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6Y2JxdG93b291c2drbGZmbmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MzE1MzIsImV4cCI6MjA1OTQwNzUzMn0.T1E1DQol6Ld2mVrarSUoEpqcNwDvKaEWTTbM_YmhDm8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
